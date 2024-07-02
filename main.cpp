40b245c0afc186b52f2dc7a5c6920702c0d70883
VishalRashmika <vishal.rashmika.perera@gmail.com>
2024-07-02 18:05:00 +0530

    new ball class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,53 @@
 #include <raylib.h>
 #include <iostream>
 
+int player_score = 0;
+int cpu_score = 0;
+
+Color green = Color{38,185,154,255};
+Color dark_green = Color{20,160,133,255};
+Color light_green = Color{129,204,184,255};
+Color yellow = Color{243,213,91,255};
+
+
+class Ball{
+public:
+    float x, y;
+    int speed_x, speed_y;
+    int radius;
+
+    void Draw(){
+        DrawCircle(x,y,radius,yellow);
+    }
+
+    void update(){
+        x += speed_x;
+        y += speed_y;
+
+        if(y + radius >= GetScreenHeight() || y - radius <= 0){
+            speed_y *= -1;
+        }
+        if (x + radius >= GetScreenWidth()){
+            cpu_score++;
+            ResetBall();
+        }
+        if(x - radius <= 0){
+            player_score++;
+            ResetBall();
+        }
+    }
+    void ResetBall(){
+        x = GetScreenWidth() / 2;
+        y = GetScreenHeight() / 2;
+
+        int speed_choices[2] = {-1,1};
+        speed_x *= speed_choices[GetRandomValue(0,1)];
+        speed_y *= speed_choices[GetRandomValue(0,1)];
+
+    }
+};
+
+
 int main(){
     std::cout << "Starting game...." << std::endl;
     const int screen_width = 1280;
